e185bb31ae2b011d0587f57ae42b0c1788cec6bc
LuisFelipeRuedaGarcia <lfxrg0724@gmail.com>
2023-06-23 22:16:46 -0500

    feat: :recycle: separete controller functions from routes
    create and export controller function as a method to import it from routes and execute it.

---
+++ Backend/Src/Controllers/Constructora.Controllers.js
@@ -0,0 +1,7 @@
+const getCosntructoras = (req,res)=>{
+    res.json({"Example":"examples..."});
+    };
+
+export const methodsHTTP = {
+    getCosntructoras /* : getCosntructoras */
+}
--- Backend/Src/Routes/Constructora.Routes.js
+++ Backend/Src/Routes/Constructora.Routes.js
@@ -1,6 +1,7 @@
 import {Router} from "express";
+import { methodsHTTP as ConstructoraControllers } from "../Controllers/Constructora.Controllers.js";
 let router = Router();
-router.get("/",(req,res)=>{
+router.get("/", ConstructoraControllers.getCosntructoras /* (req,res)=>{
 res.send("example");
-})
+} */)
 export default router;
