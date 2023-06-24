1668db696942840b65166d54c587a402553d8723
LuisFelipeRuedaGarcia <lfxrg0724@gmail.com>
2023-06-24 00:19:23 -0500

    refactor: :recycle: just save a few words
    *

--- Backend/Src/Controllers/Constructora.Controllers.js
+++ Backend/Src/Controllers/Constructora.Controllers.js
@@ -1,7 +1,7 @@
 import getConnection from "../Db/Database.js"
 const getCosntructoras = async (req,res)=>{
     const connection = await getConnection();
-    const result = await connection.query("SELECT id_constructora, nombre_constructora, nit_constructora, nombre_representante, email_contacto, telefono_contacto from constructoras");
+    const result = await connection.query("SELECT * from constructoras");
     console.log(result);
     res.json(result);
     };
