26c97f0607435f3f880bdcf44fad4d65111ef99d
LuisFelipeRuedaGarcia <lfxrg0724@gmail.com>
2023-06-24 00:17:55 -0500

    fix: :bug: fix .env variables, fetch mysql table
    fron .env fix error in name of database, fix USER for un-reserved  variable. In Controller we import connection ( promise-mysql.createConnection return) and execute .query method, SELECT * FROM. we reply it.

--- Backend/Src/Config.js
+++ Backend/Src/Config.js
@@ -5,6 +5,6 @@ config();
 export default {
     Host: process.env.HOST,
     Database : process.env.DATABASE,
-    User: process.env.USER,
+    User: process.env.USERX,
     Password: process.env.PASSWORD
 }
--- Backend/Src/Controllers/Constructora.Controllers.js
+++ Backend/Src/Controllers/Constructora.Controllers.js
@@ -1,6 +1,11 @@
-const getCosntructoras = (req,res)=>{
-    res.json({"Example":"examples..."});
+import getConnection from "../Db/Database.js"
+const getCosntructoras = async (req,res)=>{
+    const connection = await getConnection();
+    const result = await connection.query("SELECT id_constructora, nombre_constructora, nit_constructora, nombre_representante, email_contacto, telefono_contacto from constructoras");
+    console.log(result);
+    res.json(result);
     };
+
 
 export const methodsHTTP = {
     getCosntructoras /* : getCosntructoras */
